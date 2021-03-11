dbea3a9216409e330601517cd45ed73ed40283b7
Mick Labree <mick.labree@corvid.nl>
2021-03-11 14:52:47 +0100

    slidewhow toegevoegd aan mobile, alle html erin gezet voor mobile

---
+++ JS/landing.js
@@ -0,0 +1,19 @@
+var foto = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg",]
+
+var test;
+var indexSlide = 0;
+
+window.onload = function() {
+    test = document.getElementById("slideShow");
+
+    setInterval(slideShow, 2000);
+
+}
+
+function slideShow() {
+    test.style.backgroundImage = "url(img/" + foto[indexSlide] + ")";
+    indexSlide++;
+    if (indexSlide == foto.length) {
+        indexSlide = 0;
+    }
+}
